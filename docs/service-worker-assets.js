2ed535a4ecb37c9bb07e918c8b4bb51b5699b9f2
Ognjen Botica <obotica@gmail.com>
2025-09-24 15:50:57 +0200

    Release-commit

--- docs/service-worker-assets.js
+++ docs/service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "KPBBp4Z+",
+  "version": "kq3hzIGi",
   "assets": [
     {
       "hash": "sha256-q6RZv7uCmDnFxlwVJZKi3AI5BoQSUzBxMo1ZR+d/Ai8=",
@@ -62,8 +62,8 @@ self.assetsManifest = {
       "url": "_framework/Microsoft.JSInterop.WebAssembly.imfqvv3x5h.wasm"
     },
     {
-      "hash": "sha256-PVwYn5Gh7Yz2WXDNUd/Fmt2ZzZHpL8GHfvhlBd6PpDM=",
-      "url": "_framework/MudBlazorErsteIbis.nz0734ftke.wasm"
+      "hash": "sha256-HQ7xCUWKVl8AQ/zrxgNlkDk8l5D6Ed2ZRPchWJGIbx4=",
+      "url": "_framework/MudBlazorErsteIbis.xr30n9ol3z.wasm"
     },
     {
       "hash": "sha256-P6B9N3fO7t6t5v872YlnIwVE4STt5o+KpUbMJycgBrs=",
@@ -126,8 +126,8 @@ self.assetsManifest = {
       "url": "_framework/System.Runtime.InteropServices.JavaScript.f7txb1es0b.wasm"
     },
     {
-      "hash": "sha256-1ZI6R9y7Eg/odNK57CwUASBdVSr3LN07fqtYNgeZtBE=",
-      "url": "_framework/System.Runtime.b4t3zpn95a.wasm"
+      "hash": "sha256-r2FDdiGHbIh/L66ioHLuba09mAkAUTnqIp81S5pMMvY=",
+      "url": "_framework/System.Runtime.fg68vksuh7.wasm"
     },
     {
       "hash": "sha256-QPumUm/+bRPi/T+QpPMfrlCeezAnzRk6CmD/iBskgiw=",
@@ -142,7 +142,7 @@ self.assetsManifest = {
       "url": "_framework/System.Text.RegularExpressions.3ddyst6dmo.wasm"
     },
     {
-      "hash": "sha256-sO+kzGo7CyWfAti3Lx25D1qLPaGqAZDWjDJSQ9VoXWg=",
+      "hash": "sha256-H2eA/0mGKl3IfoVMaTHTfTikS8xLhxMDlVP04kuBo6I=",
       "url": "_framework/blazor.boot.json"
     },
     {
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: KPBBp4Z+ */
+/* Manifest version: kq3hzIGi */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
